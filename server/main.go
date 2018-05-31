b1f5d230a24c8396b983bb9a0bbdcc87baa04b8c
akm <akm2000@gmail.com>
2018-05-31 19:55:56 +0900

    :+1: Fix to work with appengine

--- server/main.go
+++ server/main.go
@@ -1,8 +1,11 @@
 //go:generate goagen bootstrap -d github.com/akm/goa_gae_datastore_example/design
+// +build appengine
 
 package server
 
 import (
+	"net/http"
+
 	"github.com/akm/goa_gae_datastore_example/app"
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/middleware"
@@ -23,9 +26,11 @@ func main() {
 	c := controller.NewUserController(service)
 	app.MountUserController(service, c)
 
-	// Start service
-	if err := service.ListenAndServe(":8080"); err != nil {
-		service.LogError("startup", "err", err)
-	}
+	// // Start service
+	// if err := service.ListenAndServe(":8080"); err != nil {
+	// 	service.LogError("startup", "err", err)
+	// }
 
+	// Start service
+	http.HandleFunc("/", service.Mux.ServeHTTP)
 }
